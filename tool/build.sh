7a23a5925c09ef33f3917a4e8a9515241a3da214
Anand Thakker <vestibule@anandthakker.net>
2015-04-22 16:22:07 -0400

    build script

--- tool/build.sh
+++ tool/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+rm -rf dist
 mkdir -p dist
 cp -R assets/ dist/
-node_modules/.bin/browserify app.js > dist/js/main.js
+node_modules/.bin/browserify app.js -o dist/js/main.js
