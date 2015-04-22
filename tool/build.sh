e70ebef5796a9b0dc14ed1739fb8723aecb34ee1
Anand Thakker <vestibule@anandthakker.net>
2015-04-22 16:14:38 -0400

    deployment

--- tool/build.sh
+++ tool/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+rm -rf dist
 mkdir -p dist
-cp -R assets/ dist/
-node_modules/.bin/browserify app.js > dist/js/main.js
+cp -R assets/* dist/
+ls -al dist
+node_modules/.bin/browserify app.js -o dist/js/main.js
