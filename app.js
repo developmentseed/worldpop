82f00fe874e5dcb06dea0e98d17e745934bd1649
Anand Thakker <vestibule@anandthakker.net>
2015-04-22 17:48:05 -0400

    Sync url hash with UI state.
    Closes #1

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 window.myDebug = require('debug')
 /* global L */
 require('./css/styles.css')
-var url = require('url')
+var qs = require('querystring')
 var xtend = require('xtend')
 var worldpop = require('./')
 
 var accessToken = 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJWQlQ1NlNVIn0.IT_b8KVeZDvOFZLWF7DpvQ'
 
-var defaults = {
-  source: 'devseed.oexqd7vi',
-  densityProp: 'density',
-  multiplier: 10000,
-  min_zoom: 11,
-  max_zoom: 11
-}
-var params = url.parse(window.location.href, true).query
-var options = xtend(defaults, params)
-
-function density (feature) {
-  return feature.properties[options.densityProp] / options.multiplier
-}
+/**
+ * Set up the map.
+ */
 
 document.body.innerHTML = '<div id="map"></div><div id="spinner"><div class="atebits">Calculating</div></div>'
 
@@ -30,8 +20,7 @@ var map = window.themap = L.mapbox.map('map', 'mapbox.light')
   .setView([-1.9449, 29.8806], 9)
 
 var featureGroup = L.featureGroup().addTo(map)
-
-var drawControl = new L.Control.Draw({
+new L.Control.Draw({
   edit: {
     featureGroup: featureGroup
   },
@@ -42,12 +31,25 @@ var drawControl = new L.Control.Draw({
     circle: false,
     marker: false
   }
-})
-
-drawControl.addTo(map)
+}).addTo(map)
 
 map.on('draw:created', calculateTotal)
 map.on('draw:edited', (e) => { e.layers.eachLayer(calculateTotal) })
+
+var options = parseHash()
+if (options.polygon) {
+  try {
+    options.polygon = decodeURIComponent(options.polygon)
+    L.geoJson(JSON.parse(options.polygon), {
+      onEachFeature: (feat, layer) => {
+        calculateTotal({ layer })
+      }
+    })
+  } catch(e) {
+    console.error(e)
+  }
+}
+updateHash()
 
 function calculateTotal ({layer}) {
   var tilesUri = 'tilejson+http://api.tiles.mapbox.com/v4/' +
@@ -56,6 +58,7 @@ function calculateTotal ({layer}) {
     .split('{access}').join(accessToken)
 
   var testPoly = layer.toGeoJSON()
+  updateHash(testPoly)
 
   spinner.classList.add('show')
   worldpop(options, tilesUri, density, testPoly, function (err, result) {
@@ -67,4 +70,36 @@ function calculateTotal ({layer}) {
       ${result.totalArea} (${result.polygonArea}) m^2`)
     layer.openPopup()
   })
+}
+
+function density (feature) {
+  return feature.properties[options.densityProp] / options.multiplier
+}
+
+function parseHash () {
+  var defaults = {
+    source: 'devseed.oexqd7vi',
+    densityProp: 'density',
+    multiplier: 10000,
+    min_zoom: 11,
+    max_zoom: 11
+  }
+
+  var hash = window.location.hash || '#'
+  var params = qs.parse(hash.slice(1).split(',').join('&'))
+  var options = xtend(defaults, params)
+
+  void ['multiplier', 'min_zoom', 'max_zoom']
+    .forEach((k) => options[k] = Number(options[k]))
+
+  return options
+}
+
+function updateHash (poly) {
+  if (poly) {
+    options.polygon = encodeURIComponent(JSON.stringify(poly))
+  }
+  window.location.hash = Object.keys(options)
+    .map((k) => [k, options[k]].map(encodeURIComponent).join('='))
+    .join(',')
 }
