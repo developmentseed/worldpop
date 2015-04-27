a9654622e76350e3f1fb82a1c36e15c6f801efa4
Anand Thakker <vestibule@anandthakker.net>
2015-04-27 09:05:37 -0400

    update docs

--- README.md
+++ README.md
@@ -6,18 +6,6 @@ Compute the population within a polygon from GeoJSON population data.
 Go to [http://devseed.com/worldpop](http://devseed.com/worldpop) and draw
 some polygons.
 
-## Locally
-
-Clone the repo, then:
-```
-cd worldpop
-npm install
-npm run-script develop
-```
-
-Open [http://localhost:9966](http://localhost:9966) in your browser.
-
-
 ## Node API
 ```
 var worldpop = require('worldpop')
@@ -31,4 +19,26 @@ worldpop({
 ```
 
 Read the [API docs](API.md).
+
+## Developing
+
+Clone the repo, then:
+```
+cd worldpop
+npm install
+npm run-script develop
+```
+
+This starts a [budo](https://github.com/mattdesl/budo) dev server that watches
+for changes; open [http://localhost:9966](http://localhost:9966).
+
+Run `tool/build.sh` to build the project into the `dist/` directory.
+
+The project build uses browserify to bundle javascript and PostCSS/cssnext for
+styles.  Entry points:
+
+ - index.js - main node module (e.g., the actual bean-counting logic)
+ - app.js - the web app
+ - css/styles.css - the main styles file.  It gets bundled with the javascript
+   and included into the page via app.js
 
--- index.js
+++ index.js
@@ -16,20 +16,24 @@ module.exports = worldpop
 /**
  * Computes the total population within the given polygon.
  *
- * @param opts - Options
- * @param {Number} opts.max_zoom
- * @param {Number} opts.min_zoom
- * @param {ReadableStream<Feature>|string} opts.source - A GeoJSON Feature
+ * @param opts - Options:
+ * max_zoom, min_zoom
+ *
+ * {(string|ReadableStream<Feature>)} source - A GeoJSON Feature
  * stream or Tilelive uri for the tiled population data, where each feature
  * represents an area of constant population density.
- * @param {function} opts.density - A function that accepts a feature from
+ *
+ * {function} density - A function that accepts a feature from
  * `source` and returns the population density for that feature.
- * @param {Feature<Polygon>} opts.polygon - The polygon whose interior
+ *
+ * {Feature<Polygon>} polygon - The polygon whose interior
  * population we want.
- * @param {function} opts.progress - A progress callback, called periodically
+ *
+ * {function} progress - A progress callback, called periodically
  * with the current state of {totalPopulation, totalArea, polygonArea}. (You can
  * estimate % complete with totalArea/polygonArea.)
- * @param {Number} opts.progressFrequency - Frequency (in # of features) of
+ *
+ * {Number} progressFrequency - Frequency (in # of features) of
  * progress callback.
  * @param cb - completion callback, called with {totalPopulation, totalArea,
  * polygonArea}.
